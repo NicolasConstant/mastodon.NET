c3ba07dd695447143285a83a77ebf1e9a30d36b5
Nicolas Constant <github@nicolas-constant.com>
2018-06-04 22:36:30 -0400

    Remove old .NET Framework project

--- mastodon/mastodon/Models/Application.cs
+++
@@ -1,9 +0,0 @@
-﻿// ReSharper disable InconsistentNaming
-namespace mastodon.Models
-{
-    public class Application
-    {
-        public string name { get; set; }
-        public string website { get; set; }
-    }
-}
